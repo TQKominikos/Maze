0307a71647a65c03ba839fb3a835d45b91bbb3b8
TQKominikos <JanuDominik@seznam.cz>
2021-11-22 13:59:17 +0100

    done console output

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -18,9 +18,9 @@ namespace Maze
                 for (int j = 0; j < mazeSize; j++)
                 {
                     maze[i, j] = "x";
-                    Console.Write(string.Format("{0} ",maze[i, j]));
+                    Console.Write(string.Format("{0} ", maze[i, j]));
                 }
-                Console.Write(Environment.NewLine + Environment.NewLine);
+                Console.Write(Environment.NewLine);
             }
         }
     }
